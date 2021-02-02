0425d04ec97481c14afeb3dbf32fe75068ad6932
Iqbal Tawakal <this.iqbal@gmail.com>
2021-02-02 14:52:34 +0700

    add turn

--- signaling/static/js/room.js
+++ signaling/static/js/room.js
@@ -30,7 +30,15 @@ var vm = new Vue({
         remoteStream: null,
         peerConnection: null,
         peerRemoteConnection: null,
-        pcConfig: {},
+        pcConfig: {
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:stun3.l.google.com:19302' },
+                { urls: 'stun:stun4.l.google.com:19302' },
+            ],
+        },
         username: null,
         isCameraOn: true,
         isVoiceOn: true,
